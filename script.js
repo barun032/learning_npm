d6c5e79ab6d06cdc49445e3fe90d5061f068a804
Barun Mandal <mandalbarun075@gmail.com>
2024-12-16 09:48:12 +0530

    add library inspirational-quotes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 import axios from "axios"
+import Quote from 'inspirational-quotes'
 
 axios.get('https://jsonplaceholder.typicode.com/todos/1')
   .then((response) => console.log(response.data))
+
+  console.log(Quote.getQuote());
